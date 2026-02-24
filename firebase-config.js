6023c0926b9b70c267ff3d2d2e81ef2bb9bf1ce7
pixelostia <easyswitch.dev@gmail.com>
2026-02-24 23:53:30 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,21 @@
+// firebase-config.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCihCC_yOcR_vDBFtUFz8q_kk_In7hEiKk",
+  authDomain: "void-web-c4c20.firebaseapp.com",
+  projectId: "void-web-c4c20",
+  storageBucket: "void-web-c4c20.firebasestorage.app",
+  messagingSenderId: "669855615136",
+  appId: "1:669855615136:web:879e59f33d1d29726d21c5"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);

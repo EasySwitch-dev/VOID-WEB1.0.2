4f2f79a0a0b4bfa105751b52d98764375ee6588e
pixelostia <easyswitch.dev@gmail.com>
2026-02-25 09:30:01 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,31 +6,23 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyCihCC_yOcR_vDBFtUFz8q_kk_In7hEiKk",
-    authDomain: "void-web-c4c20.firebaseapp.com",
-    projectId: "void-web-c4c20",
-    storageBucket: "void-web-c4c20.firebasestorage.app",
-    messagingSenderId: "669855615136",
-    appId: "1:669855615136:web:92e90ff435dc0be66d21c5",
-    measurementId: "G-EEWHX8F0LL"
-  };
+// CONFIG FIREBASE
+const firebaseConfig = {
+  apiKey: "AIzaSyCihCC_yOcR_vDBFtUFz8q_kk_In7hEiKk",
+  authDomain: "void-web-c4c20.firebaseapp.com",
+  projectId: "void-web-c4c20",
+  storageBucket: "void-web-c4c20.firebasestorage.app",
+  messagingSenderId: "669855615136",
+  appId: "1:669855615136:web:92e90ff435dc0be66d21c5",
+  measurementId: "G-EEWHX8F0LL"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
 
+// inizializza firebase
 const app = initializeApp(firebaseConfig);
 
+
+// esporta auth e database
 export const auth = getAuth(app);
 export const db = getFirestore(app);

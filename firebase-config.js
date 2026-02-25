2568789b59dcde460e7567849bc63cafe1a972b1
pixelostia <easyswitch.dev@gmail.com>
2026-02-25 21:11:29 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,10 @@
 // firebase-config.js
 
-// importa Firebase SDK (VERSIONE STABILE)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
-
-// CONFIGURAZIONE
 const firebaseConfig = {
   apiKey: "AIzaSyCihCC_yOcR_vDBFtUFz8q_kk_In7hEiKk",
   authDomain: "void-web-c4c20.firebaseapp.com",
@@ -16,15 +14,10 @@ const firebaseConfig = {
   appId: "1:669855615136:web:92e90ff435dc0be66d21c5"
 };
 
-
-// inizializza app
 const app = initializeApp(firebaseConfig);
 
-
-// inizializza servizi
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 
-
-// test
 console.log("Firebase inizializzato correttamente");
